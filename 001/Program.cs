78025c478c38cdf9bcb0ec20344ee4dcd156eb07
DmitryL <dimak.pax@gmail.com>
2022-12-17 15:07:26 +0600

    001

---
+++ 001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if(n1 > n2)
+{
+    Console.WriteLine($"наибольшее число: {n1} ");
+}
+else
+{
+    Console.WriteLine($"наибольшее число: {n2} ");
+}
