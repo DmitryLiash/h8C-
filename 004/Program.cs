ed9273a268e2b711090ab94f1fd1562b0ab5260c
DmitryL <dimak.pax@gmail.com>
2022-12-18 15:30:09 +0600

    004

---
+++ 004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+int chislo = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= chislo; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+
+
+
+
+
