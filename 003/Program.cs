f2c5ea69261e470f6b0ca552afd0319b5c7c597e
DmitryL <dimak.pax@gmail.com>
2022-12-18 15:17:27 +0600

    003

---
+++ 003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+int chislo = Convert.ToInt32(Console.ReadLine());
+if(chislo % 2 == 0)
+{
+    Console.WriteLine($"Это четное число: {chislo} ");
+}
+else
+{
+    Console.WriteLine($"Это нечетное число: {chislo} ");
+}
